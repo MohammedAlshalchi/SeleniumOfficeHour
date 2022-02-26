a1031bdffc2b89249e98c84be718cdac36aa6a73
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-02-25 20:21:04 -0500

    commit2

--- src/test/java/com/cydeo/tests/practice_02_23_2022/AlertExample.java
+++ src/test/java/com/cydeo/tests/practice_02_23_2022/AlertExample.java
@@ -1,5 +1,16 @@
 package com.cydeo.tests.practice_02_23_2022;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class AlertExample {
     /*
     TC-5: Confirmation alert practice
@@ -9,6 +20,62 @@ public class AlertExample {
     4. Click to OK button from the alert
     5. Verify “You clicked: Ok” text is displayed.
      */
+
+WebDriver driver;
+String url = "http://practice.cydeo.com/javascript_alerts ";
+
+
+@BeforeMethod
+public void setUp(){
+    driver= WebDriverFactory.getDriver("chrome");
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    driver.get(url);
+}
+@Test
+public void confirmAlertTest(){
+    WebElement jsConfirmBtn = driver.findElement(By.xpath("//button[.='Click for JS Confirm']"));
+    jsConfirmBtn.click();
+    Alert confirmAlert = driver.switchTo().alert();
+    confirmAlert.accept();
+
+    WebElement result = driver.findElement(By.id("result"));
+    String expected = "You clicked: Ok";
+    String actual=result.getText();
+   Assert.assertEquals(actual,expected,"Confirm Alert Result text did not appear correctly!");
+
+}
+
+
+
+
+/*
+TC-6: Information alert practice
+1. Open browser
+2. Go to website: http://practice.cydeo.com/javascript_alerts
+3. Click to “Click for JS Prompt” button
+4. Send “hello” text to alert
+5. Click to OK button from the alert
+6. Verify “You entered: hello” text is displayed.
+ */
+    @Test
+    public void promptAlertTest(){
+        WebElement jsPromptBtn = driver.findElement(By.xpath("//button[.='Click for JS Prompt']"));
+        jsPromptBtn.click();
+
+        Alert promptAlert = driver.switchTo().alert();
+        promptAlert.sendKeys("hello");
+        promptAlert.accept();// means click ok
+
+        WebElement result = driver.findElement(By.id("result"));
+        String expected = "You entered: hello";
+        String actual= result.getText();
+        Assert.assertEquals(actual,expected,"Prompt Alert Result text did not appear correctly!");
+
+    }
+
+
 
 
 
